42239f58b7f32e99ac021ef991991752457ae498
GLORIAH-B <gloriah.boss@gmail.com>
2023-08-02 13:21:09 -0700

    while?

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 unsigned int x = 0;
 listint_t *temp = head;
 
-if (temp && x < index)
+while (temp && x < index)
 {
 temp = temp->next;
 x++;
