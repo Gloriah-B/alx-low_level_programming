22cc3d508d2254f9458294d6bef19e42e08679cb
Gloriah-B <gloriah.boss@gmail.com>
2023-08-30 12:35:23 -0700

    0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/*
+ * main - function prints a string followed by new line
+ * Return 0
+ */
+
+void _puts_recursion(char *s);
+
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
+
+
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+This is aproject about recursion.
+It shows how to implement recursion
+It also shows circumstances where you should implement and where not to implement recursion
