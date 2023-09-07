5d983aae5a7fd033f1dda619ceda0adab7e8d9f6
Gloriah-B <gloriah.boss@gmail.com>
2023-09-07 10:31:17 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,71 +1,49 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * string_nconcat - Concatenates two strings up to n bytes.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of bytes from s2 to concatenate.
+ * string_nconcat_custom - Concatenates two strings up to n bytes.
+ * @str1: The first string.
+ * @str2: The second string.
+ * @n: The maximum number of bytes from str2 to concatenate.
  *
  * Return: A pointer to the concatenated string or NULL on failure.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat_custom(char *str1, char *str2, unsigned int n)
 {
 	char *result;
-	unsigned int s1_len, s2_len, result_len, i;
+	unsigned int len1, len2, result_len, i;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	if (str1 == NULL)
+		str1 = "";
+	if (str2 == NULL)
+		str2 = "";
 
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
+	for (len1 = 0; str1[len1] != '\0'; len1++)
+		;
 
-	if (n >= s2_len)
-		result_len = s1_len + s2_len;
-	else
-		result_len = s1_len + n;
+	for (len2 = 0; str2[len2] != '\0'; len2++)
+		;
 
-	result = (char *)malloc((result_len + 1) * sizeof(char));
+	if (n > len2)
+		n = len2;
+
+	result_len = len1 + n;
+
+	result = malloc(result_len + 1);
 
 	if (result == NULL)
 		return (NULL);
 
-	for (i = 0; i < s1_len; i++)
-		result[i] = s1[i];
-
-	for (unsigned int j = 0; j < n && j < s2_len; j++, i++)
-		result[i] = s2[j];
+	for (i = 0; i < result_len; i++)
+	{
+		if (i < len1)
+			result[i] = str1[i];
+		else
+			result[i] = str2[i - len1];
+	}
 
 	result[i] = '\0';
 
 	return (result);
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0 (Success).
- */
-int main(void)
-{
-	char *s1 = "Hello, ";
-	char *s2 = "World!";
-	unsigned int n = 5;
-	char *result = string_nconcat(s1, s2, n);
-
-	if (result != NULL)
-	{
-		printf("Concatenated String: %s\n", result);
-		free(result);
-	}
-	else
-	{
-		printf("Memory allocation failed!\n");
-	}
-
-	return (0);
 }
 
