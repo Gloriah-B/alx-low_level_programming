2eb70bdb286a73c783d3dd6d8cdcbb761ee6b74c
Gloriah-B <gloriah.boss@gmail.com>
2023-09-10 03:43:21 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,8 +12,8 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *concatenated_args;
-	int total_length = 0, i, j, k = 0;
+	int i, j, len = 0, total_len = 0;
+	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
@@ -21,58 +21,25 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j]; j++)
-			total_length++;
-		total_length++;
+			len++;
+		total_len += len + 1;
+		len = 0;
 	}
 
-	concatenated_args = malloc((total_length + 1) * sizeof(char));
+	str = malloc(sizeof(char) * total_len + 1);
 
-	if (concatenated_args == NULL)
+	if (str == NULL)
 		return (NULL);
 
+	len = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j]; j++)
-		{
-			concatenated_args[k] = av[i][j];
-			k++;
-		}
-		concatenated_args[k] = '\n';
-		k++;
+			str[len++] = av[i][j];
+		str[len++] = '\n';
 	}
 
-	concatenated_args[k] = '\0';
-	return (concatenated_args);
-}
-/**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
- *
- * Return: 0 on success, 1 on failure.
- */
-
-int main(int argc, char *argv[])
-{
-	char *concatenated_args;
-
-	if (argc < 2)
-	{
-		printf("Usage: %s <arg1> <arg2> ... <argN>\n", argv[0]);
-		return (1);
-	}
-
-	concatenated_args = argstostr(argc - 1, argv + 1);
-
-	if (concatenated_args == NULL)
-	{
-		printf("Memory allocation failed.\n");
-		return (1);
-	}
-
-	printf("%s", concatenated_args);
-
-	free(concatenated_args);
-	return (0);
+	str[len] = '\0';
+	return (str);
 }
 
