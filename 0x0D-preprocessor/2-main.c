2d3c17d71753c86de25a73366416f5c99cd2b9ed
Gloriah-B <gloriah.boss@gmail.com>
2023-09-11 05:19:46 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function prints name of file compiled from
+ * Return: 0
+ */
+int main(void)
+{
+    printf("Compiled from file: %s\n", __FILE__);
+    return 0;
+}
+
