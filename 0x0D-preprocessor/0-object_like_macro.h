01ed89ca1c5adf17230cfcadf845d41f17a4393c
Gloriah-B <gloriah.boss@gmail.com>
2023-09-11 05:09:26 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,16 +1,6 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
 
-/*
- * MYHEADER_H
- *
- * main - header file defines a macro named SIZE as an abbreviation for the token 1024.
- * It can be included in  C code to provide a convenient way to represent the value 1024.
- *
- * Author:Gloriah
- */
-
 #define SIZE 1024
 
-#endif /* MYHEADER_H */
-i
+#endif
