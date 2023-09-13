ed64d57e46e5f7f15bccd4daf878758a91a66d11
Gloriah-B <gloriah.boss@gmail.com>
2023-09-13 10:54:01 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - function prints a name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}
+
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+Project is about function pointers in C and everything about pointers in C
+pointers to functions
+function pointers in C/C++
+Why pointers to functions is necessary
