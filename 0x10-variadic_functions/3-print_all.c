09d6c5fdbd1dd5d3cc40a2adbe231a94e8d4f67c
Gloriah-B <gloriah.boss@gmail.com>
2023-09-14 11:58:21 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,56 +1,56 @@
 #include "variadic_functions.h"
-	#include <stdarg.h>
-	#include <stdio.h>
+#include <stdarg.h>
+#include <stdio.h>
 
+/**
+ * print_all - Prints anything based on a format string.
+ * @format: A list of types of arguments passed to the function
+ *   c: char
+ *   i: integer
+ *   f: float
+ *   s: char * (if the string is NULL, print (nil) instead)
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *sep = "";
+	int i = 0;
 
-	/**
-	 * print_all - prints anything
-	 * @format: list of types of arguments passed to the function
-	 */
-	void print_all(const char * const format, ...)
+	va_start(args, format);
+
+	while (format && format[i])
 	{
-		int i = 0;
-		char *str, *sep = "";
+		if (i > 0)
+			sep = ", ";
 
-
-		va_list list;
-
-
-		va_start(list, format);
-
-
-		if (format)
+		switch (format[i])
 		{
-			while (format[i])
-			{
-				switch (format[i])
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
+			case 's':
 				{
-					case 'c':
-						printf("%s%c", sep, va_arg(list, int));
-						break;
-					case 'i':
-						printf("%s%d", sep, va_arg(list, int));
-						break;
-					case 'f':
-						printf("%s%f", sep, va_arg(list, double));
-						break;
-					case 's':
-						str = va_arg(list, char *);
-						if (!str)
-							str = "(nil)";
-						printf("%s%s", sep, str);
-						break;
-					default:
-						i++;
-						continue;
+					char *str = va_arg(args, char *);
+
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
 				}
-				sep = ", ";
+				break;
+			default:
 				i++;
-			}
+				continue;
 		}
-
-
-		printf("\n");
-		va_end(list);
+		i++;
 	}
+
+	printf("\n");
+	va_end(args);
+}
 
