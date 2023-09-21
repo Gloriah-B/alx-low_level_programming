8518386d6b966f424a031a711da81ec004cd4f0b
Gloriah-B <gloriah.boss@gmail.com>
2023-09-21 14:31:29 -0700

    101-hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,10 @@
-section .data
-    hello db "Hello, Holberton",0x0A ; The message to be printed (with newline)
-    format db "%s",0 ; The format string for printf
-
-section .text
-    global main
-    extern printf
-
+global   main
+	  extern    printf
 main:
-    push rbp
-    mov rdi, format ; Set the format string address
-    mov rsi, hello  ; Set the message address
-    mov rax, 0      ; Clear RAX register
-    call printf     ; Call printf
-    pop rbp
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
