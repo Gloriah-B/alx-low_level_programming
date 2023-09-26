8f26b050f4a0a7dfe76f9e5b77ff0d467cafee1f
Gloriah-B <gloriah.boss@gmail.com>
2023-09-26 10:29:57 -0700

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - function handles circular lists
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *slow_ptr, *fast_ptr;
+	size_t node_count = 0;
+
+	slow_ptr = fast_ptr = head;
+
+	while (slow_ptr && fast_ptr && fast_ptr->next)
+	{
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
+
+		if (slow_ptr == fast_ptr)
+		{
+			printf("Linkedlist is a loop\n");
+			exit(98);
+		}
+
+		printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+		node_count++;
+	}
+
+	return (node_count);
+}
+
