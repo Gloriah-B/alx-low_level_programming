a2914f8e9d3bfb01a908d9d5cff3af14bac7c7d9
Gloriah-B <gloriah.boss@gmail.com>
2023-10-06 16:11:57 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,16 +3,35 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <elf.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 /**
  * print_elf_header_info - Print information from the ELF header.
- * @elf_header: A pointer to the ELF header structure.
+ * @elf_header: Pointer to the ELF header structure.
  */
+void print_elf_header_info(Elf64_Ehdr *elf_header)
+{
+	int i = 0;
 
+	printf("  Magic:\t");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x", elf_header->e_ident[i]);
+		if (i < EI_NIDENT - 1)
+			printf(" ");
+	}
+	printf("\n");
 
-void print_elf_header_info(Elf64_Ehdr *elf_header);
+	printf("  Class:\t%s\n",
+	       (elf_header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+	printf("  Data:\t\t%s\n",
+			(elf_header->e_ident[EI_DATA] == ELFDATA2LSB) ?
+			"2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:\t%d (current)\n", elf_header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:\t%d\n", elf_header->e_ident[EI_OSABI]);
+	printf("  ABI Version:\t%d\n", elf_header->e_ident[EI_ABIVERSION]);
+	printf("  Type:\t\t%d\n", elf_header->e_type);
+	printf("  Entry point address:  0x%lx\n", (unsigned long)elf_header->e_entry);
+}
 
 /**
  * main - Entry point of the program.
@@ -21,16 +40,19 @@ void print_elf_header_info(Elf64_Ehdr *elf_header);
  *
  * Return: 0 on success, 98 on error.
  */
-
 int main(int argc, char *argv[])
 {
+	int fd;
+	ssize_t bytes_read;
+	Elf64_Ehdr elf_header;
+
 	if (argc != 2)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
 		return (98);
 	}
 
-	int fd = open(argv[1], O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 
 	if (fd == -1)
 	{
@@ -38,15 +60,13 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 
-	Elf64_Ehdr elf_header;
-	ssize_t bytes_read = read(fd, &elf_header, sizeof(Elf64_Ehdr));
+	bytes_read = read(fd, &elf_header, sizeof(Elf64_Ehdr));
 
 	if (bytes_read != sizeof(Elf64_Ehdr) ||
-		elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
-		elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
-		elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
-		elf_header.e_ident[EI_MAG3] != ELFMAG3)
-	{
+	    elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
+	    elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
+	    elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
+	    elf_header.e_ident[EI_MAG3] != ELFMAG3) {
 		dprintf(STDERR_FILENO, "Error: Not an ELF file: %s\n", argv[1]);
 		return (98);
 	}
@@ -56,36 +76,5 @@ int main(int argc, char *argv[])
 
 	close(fd);
 	return (0);
-}
-/**
- * * print_elf_header_info - Print information from the ELF header.
- * @elf_header: A pointer to the ELF header structure.
- */
-void print_elf_header_info(Elf64_Ehdr *elf_header)
-{
-	printf("  Magic:   ");
-	for (int i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%02x", elf_header->e_ident[i]);
-		if (i < EI_NIDENT - 1)
-			printf(" ");
-	}
-	printf("\n");
-
-	printf("  Class:  %s\n", (elf_header->e_ident[EI_CLASS] == ELFCLASS32) ?
-	"ELF32" : "ELF64");
-	printf("  Data:   %s\n", (elf_header->e_ident[EI_DATA] == ELFDATA2LSB) ?
-		"2's complement, little endian" :
-		"2's complement, big endian");
-	printf("  Version:    %d (current)\n", elf_header->e_ident[EI_VERSION]);
-	printf("  OS/ABI: %d\n", elf_header->e_ident[EI_OSABI]);
-	printf("  ABI Version:    %d\n", elf_header->e_ident[EI_ABIVERSION]);
-	printf("  Type:   %s\n",
-		   (elf_header->e_type == ET_REL) ? "REL (Relocatable file)" :
-		   (elf_header->e_type == ET_EXEC) ? "EXEC (Executable file)" :
-		   (elf_header->e_type == ET_DYN) ? "DYN (Shared object file)" :
-		   (elf_header->e_type == ET_CORE) ? "CORE (Core file)" : "UNKNOWN");
-	printf("  Entry point address:    0x%lx\n",
-			(unsigned long)elf_header->e_entry);
 }
 
