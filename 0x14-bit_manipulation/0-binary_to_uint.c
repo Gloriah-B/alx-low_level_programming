72d4a03e6d967c214df0eeb8c6d58f795086343c
Gloriah-B <Gloriah-B@github.com>
2023-10-26 12:14:10 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,21 +13,15 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
-	if (!b)
-	{
+	if (b == NULL)
 		return (0);
-	}
 
-	while (*b)
+	for (int i = 0; b[i] != '\0'; i++)
 	{
-		if (*b != '0' && *b != '1')
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
-		result = -1;
 
-		result |= (*b == '1');
-		b++;
+		result = (result << 1) | (b[i] - '0');
 	}
 	return (result);
 }
