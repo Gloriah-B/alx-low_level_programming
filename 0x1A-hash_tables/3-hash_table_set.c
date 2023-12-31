3aef15400518b7a2f385cc2c9fedd01e2fe1fb14
Gloriah-B <Gloriah-B@github.com>
2023-12-31 10:36:12 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,68 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * create_node - creates a new hash node
- * @key: key to add the element
- * @value: value to add the element
- *
- * Return: newly created node if successful, NULL otherwise
- */
-static hash_node_t *create_node(const char *key, const char *value) {
-    hash_node_t *new_node = calloc(1, sizeof(hash_node_t));
-    if (!new_node)
-        return NULL;
-
-    new_node->key = strdup(key);
-    if (!new_node->key) {
-        free(new_node);
-        return NULL;
-    }
-
-    new_node->value = strdup(value);
-    if (!new_node->value) {
-        free(new_node->key);
-        free(new_node);
-        return NULL;
-    }
-
-    new_node->next = NULL;
-    return new_node;
-}
-
-/**
- * update_or_create_node - updates or creates a node in the hash table
- * @ht: pointer to hash table
- * @index: index to add the element
- * @key: key to add the element
- * @value: value to add the element
- *
- * Return: 1 if successful, 0 otherwise
- */
-static int update_or_create_node(hash_table_t *ht, unsigned long int index, const char *key, const char *value) 
-{
-	hash_node_t *bucket = ht->array[index];
-
-    while (bucket) {
-        if (!strcmp(key, bucket->key)) {
-            free(bucket->value);
-            bucket->value = strdup(value);
-            if (!bucket->value)
-                return 0;
-            return 1;
-        }
-        bucket = bucket->next;
-    }
-
-    hash_node_t *new_node = create_node(key, value);
-    if (!new_node)
-        return 0;
-
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-    return 1;
-}
-
-/**
  * hash_table_set - function that adds an element to the hash table
  * @ht: pointer to hash table
  * @key: key to add the element
@@ -70,22 +8,45 @@ static int update_or_create_node(hash_table_t *ht, unsigned long int index, cons
  *
  * Return: 1 if it succeeded, 0 otherwise
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value) {
-    unsigned long int index = 0;
 
-    if (!ht || !key || !*key || !value)
-        return 0;
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	char *valuecopy, *keycopy;
+	hash_node_t  *bucket, *new_node;
 
-    index = key_index((const unsigned char *)key, ht->size);
+	if (!ht || !key || !*key || !value)
+		return (0);
 
-    if (!ht->array[index]) {
-        hash_node_t *new_node = create_node(key, value);
-        if (!new_node)
-            return 0;
+	valuecopy = strdup(value);
+	if (!valuecopy)
+		return (0);
 
-        ht->array[index] = new_node;
-        return 1;
-    }
+	index = key_index((const unsigned char *)key, ht->size);
+	bucket = ht->array[index];
 
-    return update_or_create_node(ht, index, key, value);
+	while (bucket)
+	{
+		if (!strcmp(key, bucket->key))
+		{
+			free(bucket->value);
+			bucket->value = valuecopy;
+			return (1);
+		}
+		bucket = bucket->next;
+	}
+	new_node = calloc(1, sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(valuecopy);
+		return (0);
+	}
+	keycopy = strdup(key);
+	if (!keycopy)
+		return (0);
+	new_node->key = keycopy;
+	new_node->value = valuecopy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -38,8 +38,6 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-static hash_node_t *create_node(const char *key, const char *value);
-static int update_node_value(hash_node_t *node, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
@@ -87,4 +85,5 @@ void shash_table_print(const shash_table_t *ht);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
-#endif
+#endif /*_HASH_TABLES_H_*/
+
